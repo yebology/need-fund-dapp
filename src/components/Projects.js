ae3ee005c1269651b6169329a3549b49f6359be3
yebology <yobelnathaniel12@gmail.com>
2024-03-17 10:48:11 +0700

    add dummy image

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,22 +1,27 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const Projects = () => {
   return (
     <div className="flex flex-col px-6">
       <div className="flex justify-center items-center flex-wrap">
-        {Array(6)
-          .fill()
-          .map((card, i) => {
-            <ProjectCard key={i} />;
-          })}
+        {Array(6).fill().map((card, i) => (
+            <ProjectCard key={i} />
+        ))}
       </div>
     </div>
   );
 };
 
 const ProjectCard = ({ card, id }) => (
-  <div id="projects" className="rounded-lg shadow-lg bg-white">
-    Card
+  <div id="projects" className="rounded-lg shadow-lg bg-white w-64 m-4">
+    <Link to={"/projects/" + id}>
+        <img
+            src="https://www.hdwallpapers.in/download/cell_biology_background_hd_wallpaper_cellular-HD.jpg"
+            alt="title"
+            className="rounded-xl h-64 w-full object-cover"
+        />
+    </Link>
   </div>
 );
 
